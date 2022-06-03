7f6a813aa159936ac9b2967969b8a47d87ce2e4d
arthurmdros <arthurmedeiros2005@gmail.com>
2022-06-02 21:38:17 -0300

    Removendo inutilizado

--- src/utils.py
+++
@@ -1,5 +0,0 @@
-def validate_estimator(estimator):
-    """Make sure that an estimator implements the necessary methods."""
-    if not hasattr(estimator, "predict_proba"):
-        msg = "base_estimator ({}) should implement predict_proba!"
-        raise ValueError(msg.format(type(estimator).__name__))
