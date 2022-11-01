49417aaef5ab677f34e0d200ae0183c7232011f8
ArthurGorgonio <gorgonioarthur@gmail.com>
2022-11-01 00:33:41 -0300

    Sort imports correctly

--- src/detection/interfaces/__init__.py
+++ src/detection/interfaces/__init__.py
@@ -1,4 +1,5 @@
 from .chunk import Chunk
 from .drift_detector import DriftDetector
-from .threshold import Threshold
 from .statisticaltest import StatisticalTest
+from .threshold import Threshold
+
